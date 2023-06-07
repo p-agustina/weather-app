e2f5e4ec5d47c9b68fa76a4c3c8a9d0175ac4b13
AGUSTINA PATT <agustinapatt@gmail.com>
2023-06-07 22:33:15 +0100

    fixed geo location mistime

--- src/components/GeoLocation.jsx
+++ src/components/GeoLocation.jsx
@@ -2,17 +2,19 @@ import axios from "axios";
 import GEO from "../assets/icons/current-location.svg";
 
 function GeoLocation({ setLat, setLon, lat, lon, APIkey, setData }) {
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric`;
-
-  const handleCoordinates = async (e) => {
+  const handleClick = async () => {
     try {
       const position = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
       });
-  
+
+      console.log("here is the position", position);
+
       setLat(position.coords.latitude);
       setLon(position.coords.longitude);
-  
+
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${APIkey}&units=metric`;
+
       const response = await axios.get(URL);
       setData(response.data);
     } catch (error) {
@@ -22,7 +24,7 @@ function GeoLocation({ setLat, setLon, lat, lon, APIkey, setData }) {
 
   return (
     <div>
-      <button className="currentLocation" onClick={handleCoordinates}>
+      <button className="currentLocation" onClick={handleClick}>
         <img src={GEO} alt="geo location" />
       </button>
     </div>
